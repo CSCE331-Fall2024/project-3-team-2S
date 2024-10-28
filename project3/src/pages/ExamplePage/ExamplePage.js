f22a479a4046d28c9eec0f9de3e4a1eaf6904ea0
Johan <johanlanting04@gmail.com>
2024-10-28 14:38:11 -0500

    Set up folder directory structure

---
+++ project3/src/pages/ExamplePage/ExamplePage.js
@@ -0,0 +1,13 @@
+import './ExamplePage.css'
+import Logo from "../../assets/images/logo.png"
+
+function ExamplePage() {
+  return (
+    <div>
+      <h1>This is an example page.</h1>
+      <img src={Logo} />
+    </div>
+  )
+}
+
+export default ExamplePage
