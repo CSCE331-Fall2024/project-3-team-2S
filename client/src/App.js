1a915b0fd978771087f8f280ada3678ea5626aed
TimothyWng <wng.timothy@gmail.com>
2024-10-29 19:26:16 -0500

    Added InventoryPage, started management directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css'
 import ExamplePage from './pages/ExamplePage/ExamplePage'
+import InventoryPage from './pages/manager/InventoryPage'
 
 function App() {
   return (
     <div>
-      <ExamplePage />
+      <InventoryPage />
     </div>
   )
 }
