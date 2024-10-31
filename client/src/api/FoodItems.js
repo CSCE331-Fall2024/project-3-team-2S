2edabbaf5b205438c22c47907cd92a098eaff680
TimothyWng <wng.timothy@gmail.com>
2024-10-30 20:08:37 -0500

    Changed gitconfig to recognize directory name case

---
+++ client/src/api/FoodItems.js
@@ -0,0 +1,16 @@
+const API_BASE_URL = 'http://localhost:3001/api';
+
+export async function getFoodItems() {
+  try {
+    const response = await fetch(`${API_BASE_URL}/fooditems`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch food items');
+    }
+    const data = await response.json();
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.error('Error fetching food items:', error);
+    throw error;
+  }
+}
