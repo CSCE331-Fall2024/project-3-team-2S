d62720305cae04e32a922f74fe3ad1072f8f6a6d
TimothyWng <wng.timothy@gmail.com>
2024-11-02 22:57:35 -0500

    Created InventoryTable component and implemented into Inventory Page

--- client/src/pages/manager/InventoryPage.js
+++ client/src/pages/manager/InventoryPage.js
@@ -1,19 +1,23 @@
 import './InventoryPage.css'
 import Logo from "../../assets/images/logo.png"
+import InventoryTable from '../../components/InventoryTable/InventoryTable';
 
-function ExamplePage() {
+function InventoryPage() {
   return (
     <div>
       <div class="header-container">
         <img src={Logo} />
         <h1>Inventory</h1>
-        <button>Sign Out</button>
+        <button className='sign-out-button'>Sign Out</button>
       </div>
       <div class="inventory-body">
+        <InventoryTable />
+      </div>
+      <div className='details-panel'>
 
       </div>
     </div>
   )
 }
 
-export default ExamplePage
+export default InventoryPage
