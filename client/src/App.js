e414f3a7b674d6eaab80b5ac560bf2ffce42c9be
Johan <johanlanting04@gmail.com>
2024-11-07 13:12:32 -0600

    Set up basic CashierOrderPage

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,24 @@ import NewOrderPage from './pages/Customer/NewOrderPage/NewOrderPage'
 import FoodItemPage from './pages/Customer/FoodItemPage/FoodItemPage'
 import CheckoutPage from './pages/Customer/CheckoutPage/CheckoutPage'
 import InventoryPage from './pages/manager/InventoryPage'
+import CashierOrderPage from './pages/Cashier/NewOrderPage/CashierOrderPage'
 import { OrderProvider } from './context/OrderContext'
 import './App.css'
 
 function App() {
   return (
-    <OrderProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<StartPage />} />
-          <Route path="/new-order" element={<NewOrderPage />} />
-          <Route path="/food-item" element={<FoodItemPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/inventory" element={<InventoryPage />} />
-        </Routes>
-      </Router>
-    </OrderProvider>
-
+    // <OrderProvider>
+    //   <Router>
+    //     <Routes>
+    //       <Route path="/" element={<StartPage />} />
+    //       <Route path="/new-order" element={<NewOrderPage />} />
+    //       <Route path="/food-item" element={<FoodItemPage />} />
+    //       <Route path="/checkout" element={<CheckoutPage />} />
+    //       <Route path="/inventory" element={<InventoryPage />} />
+    //     </Routes>
+    //   </Router>
+    // </OrderProvider>
+    <CashierOrderPage />
   )
 }
 
