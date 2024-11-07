8779975e89c42e93260d935f993018a75f4ad36b
Johan <johanlanting04@gmail.com>
2024-11-07 16:51:15 -0600

    Make food item buttons render based on selected menu item type

--- client/src/components/CashierReceipt/CashierReceipt.js
+++ client/src/components/CashierReceipt/CashierReceipt.js
@@ -1,29 +1,62 @@
-import './CashierReceipt.css'
-import CashierReceiptItem from '../CashierReceiptItem/CashierReceiptItem'
+import './CashierReceipt.css';
+import { useState } from 'react';
+import CashierReceiptItem from '../CashierReceiptItem/CashierReceiptItem';
+import { useCashierOrderContext } from '../../context/CashierOrderContext';
 
 function CashierReceipt() {
+  const { selectedReceiptItem } = useCashierOrderContext();
+
+  const [receiptItems, setReceiptItems] = useState([
+    {
+      id: 1,
+      menuItemType: "Plate",
+      side: "Fried Rice",
+      entrees: ["Orange Chicken", "Grilled Teriyaki"],
+      alacarte: null,
+      appetizer: null,
+      drink: null,
+      price: 3,
+    },
+    {
+      id: 2,
+      menuItemType: "Appetizer",
+      side: null,
+      entrees: ["Mushroom Chicken"],
+      alacarte: null,
+      appetizer: null,
+      drink: null,
+      price: 5,
+    },
+    {
+      id: 3,
+      menuItemType: "Appetizer",
+      side: null,
+      entrees: ["Mushroom Chicken"],
+      alacarte: null,
+      appetizer: null,
+      drink: null,
+      price: 5,
+    },
+  ]);
+
   return (
     <div className="cashier-receipt">
-      <CashierReceiptItem
-        menuItemType="Plate"
-        side="Fried Rice"
-        entrees={["Orange Chicken, Grilled Teriyaki"]}
-        alacarte={null}
-        appetizer={null}
-        drink={null}
-        price={3}
-      />
-      <CashierReceiptItem
-        menuItemType="Appetizer"
-        side={null}
-        entrees={["Mushroom Chicken"]}
-        alacarte={null}
-        appetizer={null}
-        drink={null}
-        price={5}
-      />
+      {receiptItems.map((item) => (
+        <CashierReceiptItem
+          key={item.id}
+          id={item.id}
+          menuItemType={item.menuItemType}
+          side={item.side}
+          entrees={item.entrees}
+          alacarte={item.alacarte}
+          appetizer={item.appetizer}
+          drink={item.drink}
+          price={item.price}
+          selected={selectedReceiptItem?.id === item.id} // Check unique id for selection
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default CashierReceipt
+export default CashierReceipt;
--- client/src/pages/Cashier/CashierOrderPage/CashierOrderPage.js
+++ client/src/pages/Cashier/CashierOrderPage/CashierOrderPage.js
@@ -1,8 +1,10 @@
-import './CashierOrderPage.css'
-import Logo from "../../../assets/images/logo.png"
-import CashierReceipt from '../../../components/CashierReceipt/CashierReceipt'
+import './CashierOrderPage.css';
+import Logo from "../../../assets/images/logo.png";
+import CashierReceipt from '../../../components/CashierReceipt/CashierReceipt';
+import { useCashierOrderContext } from '../../../context/CashierOrderContext';
 
 function CashierOrderPage() {
+  const { currentMenuItemType } = useCashierOrderContext();
 
   return (
     <div className="cashier-order-page-container">
@@ -16,39 +18,51 @@ function CashierOrderPage() {
             <button>New Appetizer</button>
             <button>New Drink</button>
           </div>
-          <div className="cashier-sides">
-            <h3>Sides</h3>
-            <div className="cashier-btns">
-              <button>White Rice</button>
-              <button>Fried Rice</button>
-              <button>Super Greens</button>
+
+          {['Bowl', 'Plate', 'Bigger Plate', 'A La Carte'].includes(currentMenuItemType) && (
+            <>
+              <div className="cashier-sides">
+                <h3>Sides</h3>
+                <div className="cashier-btns">
+                  <button>White Rice</button>
+                  <button>Fried Rice</button>
+                  <button>Super Greens</button>
+                </div>
+              </div>
+              <div className="cashier-entrees">
+                <h3>Entrees</h3>
+                <div className="cashier-btns">
+                  <button>Orange Chicken</button>
+                  <button>Teriyaki</button>
+                  <button>Mushroom Chicken</button>
+                </div>
+              </div>
+            </>
+          )}
+
+          {currentMenuItemType === 'Appetizer' && (
+            <div className="cashier-appetizers">
+              <h3>Appetizers</h3>
+              <div className="cashier-btns">
+                <button>Cream Cheese Rangoon</button>
+                <button>Apple Pie Roll</button>
+                <button>Apple Slices</button>
+              </div>
             </div>
-          </div>
-          <div className="cashier-entrees">
-            <h3>Entrees</h3>
-            <div className="cashier-btns">
-              <button>Orange Chicken</button>
-              <button>Teriyaki</button>
-              <button>Mushroom Chicken</button>
+          )}
+
+          {currentMenuItemType === 'Drink' && (
+            <div className="cashier-drinks">
+              <h3>Drinks</h3>
+              <div className="cashier-btns">
+                <button>Aquafina</button>
+                <button>Dr Pepper</button>
+                <button>Sweet Tea</button>
+              </div>
             </div>
-          </div>
-          <div className="cashier-appetizers">
-            <h3>Appetizers</h3>
-            <div className="cashier-btns">
-              <button>Cream Cheese Rangoon</button>
-              <button>Apple Pie Roll</button>
-              <button>Apple Slices</button>
-            </div>
-          </div>
-          <div className="cashier-drinks">
-            <h3>Drinks</h3>
-            <div className="cashier-btns">
-              <button>Aquafina</button>
-              <button>Dr Pepper</button>
-              <button>Sweet Tea</button>
-            </div>
-          </div>
+          )}
         </div>
+
         <div className="cashier-receipt-container">
           <CashierReceipt />
         </div>
@@ -59,7 +73,7 @@ function CashierOrderPage() {
         <button>Pay</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default CashierOrderPage
+export default CashierOrderPage;
