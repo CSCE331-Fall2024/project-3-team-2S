cc5c8cd22ec9373e3bc268b6e85c468299c707c7
TimothyWng <wng.timothy@gmail.com>
2024-11-16 16:41:44 -0600

    Set up basic routing and new template pages for other manager pages

--- client/src/pages/manager/OrderHistoryPage/OrderHistoryPage.js
+++ client/src/pages/manager/OrderHistoryPage/OrderHistoryPage.js
@@ -1,46 +1,15 @@
 import React, { useState } from 'react';
-import './EmployeePage.css'; // Make sure to create this CSS file or rename it from InventoryPage.css
+import './OrderHistoryPage.css';
 import { useNavigate } from 'react-router-dom'; // Import for routing
 import Logo from "../../../assets/images/logo.png";
-import EmployeeTable from '../../../components/EmployeeTable/EmployeeTable'; // Import EmployeeTable component
-import EmployeeDetails from '../../../components/EmployeeDetails/EmployeeDetails'; // Import EmployeeDetails component
-import AddEmployeePanel from '../../../components/AddEmployeePanel/AddEmployeePanel'; // Import AddEmployeePanel component
 import ManagerProfileDropdown from '../../../components/ManagerProfileDropdown/ManagerProfileDropdown'; // Import ManagerProfile component
 
-function EmployeePage() {
+function OrderHistoryPage() {
   const [selectedItem, setSelectedItem] = useState(null);
-  const [employeeData, setEmployeeData] = useState([]);
-  const [activePage, setActivePage] = useState("Employees"); // Track the active page
+  const [inventoryData, setInventoryData] = useState([]);
+  const [activePage, setActivePage] = useState("Order History"); // Track the active page
   
   const navigate = useNavigate(); // Initialize navigate for routing
-
-  // Handle selecting an item from the table
-  const handleSelectItem = (item) => {
-    setSelectedItem(item);
-  };
-
-  // Handle updating an existing item
-  const handleItemUpdate = (updatedItem) => {
-    setEmployeeData(prevData => 
-      prevData.map(item => 
-        item.employeeid === updatedItem.employeeid ? updatedItem : item
-      )
-    );
-    setSelectedItem(updatedItem);
-  };
-
-  // Handle deleting an item
-  const handleItemDelete = (deletedItemId) => {
-    setEmployeeData(prevData => 
-      prevData.filter(item => item.employeeid !== deletedItemId)
-    );
-    setSelectedItem(null);
-  };
-
-  // Handle adding a new employee
-  const handleNewEmployeeAdd = (newItem) => {
-    setEmployeeData(prevData => [...prevData, newItem]); // Add the new employee to the employee list
-  };
 
   // Handle navigation clicks
   const handleNavClick = (text) => {
@@ -70,7 +39,7 @@ function EmployeePage() {
         
         {/* Manager Navigation */}
         <div className='manager-nav'>
-        <span 
+          <span 
             onClick={() => handleNavClick("Order History")}
             className={activePage === "Order History" ? "active-nav" : ""}
           >
@@ -110,32 +79,8 @@ function EmployeePage() {
         </div>
       </div>
 
-      <div className="employee-container">
-        <div className="employee-header">
-          <h1>Employees</h1>
-          <div className="employee-body">
-            <EmployeeTable 
-              data={employeeData} 
-              setData={setEmployeeData} 
-              onSelectItem={handleSelectItem} 
-            />
-          </div>
-        </div>
-
-        <EmployeeDetails 
-          selectedItem={selectedItem} 
-          onItemUpdate={handleItemUpdate}
-          onItemDelete={handleItemDelete}
-        />
-
-        <div className="divider"></div>
-
-        <AddEmployeePanel 
-          onItemAdd={handleNewEmployeeAdd} 
-        />
-      </div>
     </div>
   );
 }
 
-export default EmployeePage;
+export default OrderHistoryPage;
