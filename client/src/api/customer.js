31d0ef68f481404eaa74f4848742bfaac11ae03f
TimothyWng <wng.timothy@gmail.com>
2024-11-17 03:27:35 -0600

    Updated API_BASE_URL to match .env

--- client/src/api/customer.js
+++ client/src/api/customer.js
@@ -1,6 +1,6 @@
 // API/customer.js
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export async function getCustomers() {
   const sql = `
