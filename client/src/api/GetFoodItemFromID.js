d5df54569f37cec4afd8ee85e7bfbaf265b26464
Johan <johanlanting04@gmail.com>
2024-11-19 16:03:43 -0600

    Implement food item details modal
    Shows food item details when clicking FoodItemBtn

--- client/src/api/GetFoodItemFromID.js
+++ client/src/api/GetFoodItemFromID.js
@@ -1,13 +1,13 @@
-const API_BASE_URL = 'https://project-3-team-2-s-dep-server.vercel.app/api';
+const API_BASE_URL = 'http://localhost:3001';
 
 export async function getFoodItemFromID(foodID) {
   try {
-    const response = await fetch(`${API_BASE_URL}/fooditems/${foodID}`);
+    const response = await fetch(`${API_BASE_URL}/api/fooditems/${foodID}`);
     if (!response.ok) {
       throw new Error('Failed to fetch food item');
     }
     const data = await response.json();
-    return data.name
+    return data
   } catch (error) {
     console.error('Error fetching food items:', error);
     throw error;
